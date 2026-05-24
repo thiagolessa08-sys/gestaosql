e22c75b65a2a74429d1af0a7ed81d3e894e6e1d0
thiagolessa <capitani28@exa.com.br>
2026-05-24 11:05:03 -0300

    fix: remove unnecessary any cast in projetos page

--- src/app/(app)/projetos/page.tsx
+++ src/app/(app)/projetos/page.tsx
@@ -43,7 +43,7 @@ export default async function ProjetosPage() {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {projects.map((project) => (
-            <ProjectCard key={project.id} project={project as any} />
+            <ProjectCard key={project.id} project={project} />
           ))}
         </div>
       )}
